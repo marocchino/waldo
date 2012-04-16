36145985fb14967ac52692ccea6ab2d954e7d10e
Tw. Shim <marocchino@gmail.com>
2012-04-17 01:18:34 +0900

    change framework to socket stream

--- server.js
+++ server.js
@@ -1,5 +1,33 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\nApp (waldo) is running on Node.JS ' + process.version);
-}).listen(process.env['app_port'] || 3000);
+// My SocketStream app
+
+var http = require('http')
+  , ss = require('socketstream');
+
+// Define a single-page client
+ss.client.define('main', {
+  view: 'app.html',
+  css:  ['libs', 'app.styl'],
+  code: ['libs', 'app'],
+  tmpl: '*'
+});
+
+// Serve this client on the root URL
+ss.http.route('/', function(req, res){
+  res.serveClient('main');
+})
+
+// Code Formatters
+ss.client.formatters.add(require('ss-stylus'));
+
+// Use server-side compiled Hogan (Mustache) templates. Others engines available
+ss.client.templateEngine.use(require('ss-hogan'));
+
+// Minimize and pack assets if you type: SS_ENV=production node app.js
+if (ss.env == 'production') ss.client.packAssets();
+
+// Start web server
+var server = http.Server(ss.http.middleware);
+server.listen(3000);
+
+// Start SocketStream
+ss.start(server);
